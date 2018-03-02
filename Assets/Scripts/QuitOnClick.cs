be4bef2716e52a146bce5f4c506d1fffb81c93e2
koszmarek <mkotajny@gmail.com>
2018-03-02 09:10:42 +0100

    First, Initial commit: - empty Unity project - "Point Reaction" created - first game menu added

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+    public void Quit()
+    {
+        Application.Quit();
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;        
+        #endif
+    }
+}
