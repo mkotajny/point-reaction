148a8b29d81dd7a1159c3adf28aaf396d6452352
koszmarek <mkotajny@gmail.com>
2018-03-03 22:29:21 +0100

    PR-3 implementation - "Amount of points" dropdown list

---
+++ Assets/Scripts/UI/SettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+    public string keyName;
+    public Text keyValue;
+    public Dropdown dropDown; 
+
+    public void Start()
+    {
+        GetDropdownValue();
+    }
+
+    public void SetDropdownValue()
+    {
+        PlayerPrefs.SetString(keyName, keyValue.text);
+    }
+
+    public void GetDropdownValue()
+    {
+        string dropDownValueText = PlayerPrefs.GetString(keyName);
+        int dropDownValue;
+        int.TryParse(dropDownValueText, out dropDownValue);
+        dropDown.value = dropDownValue - 1;
+    }
+}
