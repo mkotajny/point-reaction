3c3700a2950e166ef39b6f453df6830737ffca5e
koszmarek <mkotajny@gmail.com>
2018-03-05 08:46:37 +0100

    PR-15 - implementation of the Story

--- Assets/Scripts/UI/DropDownSaver.cs
+++ Assets/Scripts/UI/DropDownSaver.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsManager : MonoBehaviour {
+public class DropDownSaver : MonoBehaviour {
 
     public string keyName;
     public Text keyValue;
---
+++ Assets/Scripts/UI/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public void LoadByIndex (int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
