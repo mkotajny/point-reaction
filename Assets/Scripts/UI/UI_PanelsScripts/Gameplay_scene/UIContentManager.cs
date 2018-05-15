8dfe1eb1d44620d1ba41f792b23ebef6f8e80648
koszmarek <mkotajny@gmail.com>
2018-05-15 07:57:30 +0200

    PR-47- implementation of the fix described in the ticket

--- Assets/Scripts/UI/UI_PanelsScripts/Gameplay_scene/UIContentManager.cs
+++ Assets/Scripts/UI/UI_PanelsScripts/Gameplay_scene/UIContentManager.cs
@@ -15,16 +15,13 @@ public class UIContentManager : MonoBehaviour {
         _levelStartPanel = GameObject.Find("Panel_Start");
         _panelStartLevelValue = GameObject.Find("PanelStartLevelValue").GetComponent<Text>();
         _panelStart_PointsLivingTime = GameObject.Find("PanelStart_PointsLivingTime").GetComponent<Text>();
-
         _levelResultPanel = GameObject.Find("Panel_Result");
         _panelResultLevelValue = GameObject.Find("PanelResultLevelValue").GetComponent<Text>();
         _levelResultQtyPointsHit = GameObject.Find("QtyOfPointsHit_value").GetComponent<Text>();
         _levelResultReactionAvg = GameObject.Find("AvgReaction_value").GetComponent<Text>();
         _levelResultReactionFastest = GameObject.Find("FastestReaction_value").GetComponent<Text>();
-
         _levelResultText = GameObject.Find("LevelResult_value").GetComponent<Text>();
         _resultLevelButtonText = GameObject.Find("PanelResult_ButtonRun_Text").GetComponent<Text>();
-
 
         _levelResultPanel.SetActive(false);
     }
@@ -56,8 +53,13 @@ public class UIContentManager : MonoBehaviour {
         _panelStart_PointsLivingTime.text = GameMode_1.CurrentLevel.PointsLivingTimer.Lenght.ToString() + " sec";
         _panelResultLevelValue.text = GameMode_1.CurrentLevel.LevelNo.ToString();
         _levelResultQtyPointsHit.text = GameMode_1.CurrentLevel.HitsQty.ToString();
-        _levelResultReactionAvg.text = GameMode_1.CurrentLevel.ReactionAvg.ToString("0.00") + " sec";
-        _levelResultReactionFastest.text = GameMode_1.CurrentLevel.ReactionFastest.ToString("0.00") + " sec";
+
+        if (GameMode_1.CurrentLevel.HitsQty > 0) {
+            _levelResultReactionAvg.text = GameMode_1.CurrentLevel.ReactionAvg.ToString("0.00") + " sec";
+            _levelResultReactionFastest.text = GameMode_1.CurrentLevel.ReactionFastest.ToString("0.00") + " sec"; }
+        else {
+            _levelResultReactionAvg.text = "-";
+            _levelResultReactionFastest.text = "-"; }
 
         if (GameMode_1.CurrentLevel.PlayStatus == LevelPlayStatuses.Win)
         {
