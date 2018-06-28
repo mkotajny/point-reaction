6deb1834d5c02df11a1d1f36139a8a9fc96ec5aa
koszmarek <mkotajny@gmail.com>
2018-06-28 09:24:28 +0200

    PR-67 - simple autologin to googleplay

--- Assets/Scripts/UI/UI_PanelsScripts/MainMenu_scene/MainMenu.cs
+++ Assets/Scripts/UI/UI_PanelsScripts/MainMenu_scene/MainMenu.cs
@@ -1,18 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
 
 public class MainMenu : MonoBehaviour {
 
+
     void Awake ()
     {
-        ActivityLogger.InitializeLog();
         Screen.orientation = ScreenOrientation.Portrait;
+        ActivityLogger.InitializeLog();
+
         GameObject.Find("VersionNumber_text").GetComponent<Text>().text
-            = "Version " + Application.version;
-	}
+        = "Version " + Application.version;
+
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        Debug.Log("debug: Sign in");
+        Social.localUser.Authenticate(success => {
+            GameObject.Find("PlayerName_text").GetComponent<Text>().text 
+            = "Player: " + Social.localUser.userName + ".";
+            Debug.Log("debug: Sign in succeded");
+        });
+
+        /*
+        try
+        {
+
+            PlayGamesPlatform.Instance.SignOut();
+            Debug.Log("debug: Sign out succeded");
+        }
+        catch
+        {
+            Debug.Log("debug: Sign out failed");
+        }
+
+        Social.localUser.Authenticate(success => {
+            Debug.Log("debug: signet in again");
+        });
+        */
+
+    }
 
     void OnEnable()
     {
         //ActivityLogger.AddLogLine("MAIN MENU panel has been opened");
     }
+
 }
