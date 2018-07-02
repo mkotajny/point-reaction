8d9009990c259e457fac07a3d2c063ad64df29b9
koszmarek <mkotajny@gmail.com>
2018-07-02 09:41:33 +0200

    PR-67 - adding UI for google play - sign in / singn out

--- Assets/Scripts/UI/UI_PanelsScripts/MainMenu_scene/PlayerSettings.cs
+++ Assets/Scripts/UI/UI_PanelsScripts/MainMenu_scene/PlayerSettings.cs
@@ -4,8 +4,18 @@ using UnityEngine.UI;
 
 public class PlayerSettings : MonoBehaviour {
 
-    Text _levelDropdownValue;
+    Text _levelDropdownValue, _playerName;
     Dropdown _levelDropDown;
+    UiControler _uiControler;
+    GameObject _signInButton, _signOutButton;
+
+    bool PlayerAuthenticated
+    {
+        get
+        {
+            return (string.IsNullOrEmpty(_uiControler.PlayerName.text)) ? false : true;
+        }
+    }
 
     int _currentLevelNo, _bestLevelNo;
 
@@ -13,33 +23,68 @@ public class PlayerSettings : MonoBehaviour {
     {
         _levelDropdownValue = GameObject.Find("LevelDropdownLabel").GetComponent<Text>();
         _levelDropDown = GameObject.Find("LevelDropdown").GetComponent<Dropdown>();
+        _uiControler = GameObject.Find("UiControler").GetComponent<UiControler>();
+        _signInButton = GameObject.Find("ButtonSignIn");
+        _signOutButton = GameObject.Find("ButtonSignOut");
+        _playerName = GameObject.Find("PlayerSettings_PlayerName_text").GetComponent<Text>();
 
+        SwitchGoogleSignButtons();
+        GenerateListOfLevels();
+    }
+
+    #region Google Play Account Management
+
+
+    private void Update()
+    {
+        if (_signInButton.activeInHierarchy == PlayerAuthenticated)
+            SwitchGoogleSignButtons();
+    }
+
+    public void GooglePlaySignInOut(bool signIn = true)
+    {
+        if (signIn)
+            _uiControler.GooglePlaySignIn();
+        else
+            _uiControler.GooglePlaySignOut();
+    }
+
+    public void SwitchGoogleSignButtons()
+    {
+        if (PlayerAuthenticated)
+        {
+            _playerName.text = _uiControler.PlayerName.text;
+            _signInButton.SetActive(false);
+        }
+        else
+        {
+            _playerName.text = "-";
+            _signInButton.SetActive(true);
+        }
+
+        _signOutButton.SetActive(!_signInButton.activeInHierarchy);
+    }
+    #endregion
+
+    #region Current Level Selection
+    void GenerateListOfLevels()
+    {
         _currentLevelNo = ConvertionTools.GestIntFromString(PlayerPrefs.GetString("LevelNo"), 0);
         _bestLevelNo = ConvertionTools.GestIntFromString(PlayerPrefs.GetString("BestLevelNo"), -1);
         if (_bestLevelNo == -1)
             _bestLevelNo = _currentLevelNo;
 
-        GenerateListOfOptions();
-        _levelDropDown.value = _currentLevelNo;
-    }
-
-    void OnEnable()
-    {
-        //ActivityLogger.AddLogLine("PLAYER SETTINGS menu panel has been opened");
-    }
-
-
-    void GenerateListOfOptions()
-    {
         List<string> options = new List<string>();
         for (int i = 0; i <= _bestLevelNo; i++)
             options.Add(i.ToString());
 
         _levelDropDown.AddOptions(options);
+        _levelDropDown.value = _currentLevelNo;
     }
 
     public void SetDropdownValue()
     {
         PlayerPrefs.SetString("LevelNo", _levelDropdownValue.text);
     }
+    #endregion
 }
---
+++ Assets/Scripts/UI/UI_PanelsScripts/MainMenu_scene/UiControler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+
+public class UiControler : MonoBehaviour {
+
+    public Text PlayerName;
+
+
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        ActivityLogger.InitializeLog();
+
+        PlayerName = GameObject.Find("PlayerName_background").GetComponent<Text>();
+        GameObject.Find("VersionNumber_text").GetComponent<Text>().text
+        = "Version " + Application.version;
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        if (PlayerPrefs.GetInt("InGooglePlay") == 1)
+            GooglePlaySignIn();
+    }
+
+
+    public void GooglePlaySignIn()
+    {
+        Social.localUser.Authenticate(success =>
+        {
+            PlayerName.text = Social.localUser.userName;
+            PlayerPrefs.SetInt("InGooglePlay", 1);
+        });
+    }
+
+    public void GooglePlaySignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        PlayerName.text = string.Empty;
+        PlayerPrefs.SetInt("InGooglePlay", 0);
+    }
+}
